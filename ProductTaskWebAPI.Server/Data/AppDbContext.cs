c1e569a55634d08f6b30a3a64671b1eec93d9bc6
Eray Sönmezer <eray.sonmezer@hotmail.com>
2025-01-18 15:57:35 +0300

    Created the AppDbContext class under the Data folder, added the ConnectionStrings configuration in the appsettings.json file, and configured the database context in the Program.cs file.

---
+++ ProductTaskWebAPI.Server/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductTaskWebAPI.Server.Models;
+
+namespace ProductTaskWebAPI.Server.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
